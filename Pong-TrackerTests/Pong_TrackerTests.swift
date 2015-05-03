2c1d5dd5d1c225d79d4c25fdb077d696340b053f
Christian Gossain <christian@showbie.com>
2015-05-03 16:11:29 -0600

    Initial Commit

---
+++ Pong-TrackerTests/Pong_TrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pong_TrackerTests.swift
+//  Pong-TrackerTests
+//
+//  Created by Christian R. Gossain on 2015-05-03.
+//  Copyright (c) 2015 Christian R. Gossain. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Pong_TrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
